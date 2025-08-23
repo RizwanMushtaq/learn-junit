abf9716a8057670b43da445704e431b527ce72d9
Rizwan <rizwanmushtaq15@gmail.com>
2025-08-23 10:46:29 +0200

    added annotations to use Mockito

--- src/test/java/com/rizwanmushtaq/services/GSTServiceTest.java
+++ src/test/java/com/rizwanmushtaq/services/GSTServiceTest.java
@@ -2,15 +2,32 @@ package com.rizwanmushtaq.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class GSTServiceTest {
+  @Mock
+  private GSTProvider mockProvider;
+  @InjectMocks
+  private GSTService gstService;
+
   @Test
   public void gstCalculatorTest() {
-    GSTProvider mockProvider = Mockito.mock(GSTProvider.class);
-    GSTService gstService = new GSTService(mockProvider);
-    Mockito.when(mockProvider.getGSTDetail()).thenReturn(18);
-    int actualResult = gstService.calculateGST();
+    GSTProvider mockProvider1 = Mockito.mock(GSTProvider.class);
+    GSTService gstService1 = new GSTService(mockProvider1);
+    Mockito.when(mockProvider1.getGSTDetail()).thenReturn(18);
+    int actualResult = gstService1.calculateGST();
     Assertions.assertEquals(18, actualResult);
+  }
+
+  @Test
+  public void gstCalculatorTest2() {
+    Mockito.when(mockProvider.getGSTDetail()).thenReturn(10);
+    int actualResult = gstService.calculateGST();
+    Assertions.assertEquals(10, actualResult);
   }
 }
